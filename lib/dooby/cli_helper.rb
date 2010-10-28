6cbf3b16830960b897b0d386cd8daa1ddaf71308
Rafael Magana <raf.magana@gmail.com>
2010-10-28 12:43:32 -0500

    cli helper

---
+++ lib/dooby/cli_helper.rb
@@ -0,0 +1,31 @@
+module Dooby
+  class CLIHelper
+    
+    DEL_TASKS = "Sure you want to delete all the taks???".red_on_white.bold
+    TRASH = "Sure you want to delete the .dooby directory???".red_on_white.bold
+    
+    def self.flush?
+      agree SURE_DEL_TAKS, true      
+    end
+    
+    def self.trash?
+      agree TRASH, true
+    end
+    
+    def self.keep_asking(question, autocompletion = nil)
+      Readline.completion_append_character = " "
+      Readline.completion_proc = proc { |s| autocompletion.grep( /^#{Regexp.escape(s)}/ ) }
+      
+      stty_save = `stty -g`.chomp
+      
+      while value = Readline.readline(question)
+          yield value.chomp.strip
+      end      
+
+      rescue Interrupt
+        system("stty", stty_save)
+        exit
+
+    end
+  end
+end
