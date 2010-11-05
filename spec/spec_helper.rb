ff418da010e378f96dcf99617017d409e2a99d0b
Rafael Magana <raf.magana@gmail.com>
2010-11-05 04:08:21 -0600

    add fake todo and tasks for testing purposes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,16 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
 
 require 'dooby'
+
+def fake_todo_text
+  "#context @person %project :status"
+end
+
+def fake_tasks
+  tasks = {}
+  3.times do |t| 
+    task = Dooby::Task.new fake_todo_text + t.to_s
+    tasks[task.id] = task
+  end
+  tasks
+end
